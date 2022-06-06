2bce70c7a3d6481e518d076a6b942574f2490a03
Gomgom Immanuel <68519252+gomgomimmanuel@users.noreply.github.com>
2022-06-06 08:49:21 +0700

    Add files via upload

---
+++ myfirstprogram1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hi! \n";
+    cout << "Call me Gomgom Immanuel \n";
+    return 0;
+}
---
+++ myfirstprogram2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Code by Gomgom Immanuel
+    cout << "Hi!" << endl;
+    cout << "Call me Gomgom Immanuel \n";
+    return 0;
+}
